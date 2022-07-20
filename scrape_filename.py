1e825b5318a3ae8bd69df19e8ec1ba48d26d7318
scott967 <scott967@users.noreply.github.com>
2022-07-20 11:56:20 -1000

    Update for matrix / python 3

--- scrape_filename.py
+++ scrape_filename.py
@@ -1,35 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-import xbmcplugin,xbmcgui,xbmc,xbmcaddon
-import os,sys,urllib
+import sys
+import urllib.parse as urllib
+
+import xbmcgui
+import xbmcplugin
+
 
 def get_params():
-        param=[]
-        paramstring=sys.argv[2]
-        if len(paramstring)>=2:
-                params=sys.argv[2]
-                cleanedparams=params.replace('?','')
-                if (params[len(params)-1]=='/'):
-                        params=params[0:len(params)-2]
-                pairsofparams=cleanedparams.split('&')
-                param={}
-                for i in range(len(pairsofparams)):
-                        splitparams={}
-                        splitparams=pairsofparams[i].split('=')
-                        if (len(splitparams))==2:
-                                param[splitparams[0]]=splitparams[1]
-                                
-        return param
-
+    param = []
+    paramstring = sys.argv[2]
+    if len(paramstring) >= 2:
+        params = sys.argv[2]
+        cleanedparams = params.replace('?','')
+        if params[len(params)-1] == '/':
+            params = params[0:len(params)-2]
+        pairsofparams = cleanedparams.split('&')
+        param = {}
+        for i in range(len(pairsofparams)):
+            splitparams = {}
+            splitparams = pairsofparams[i].split('=')
+            if (len(splitparams)) == 2:
+                param[splitparams[0]] = splitparams[1]
+    return param
 
 params=get_params()
-
 action=urllib.unquote_plus(params["action"])
-
 if action == 'find':
     title=urllib.unquote_plus(params["title"])
-    liz=xbmcgui.ListItem(title, thumbnailImage='DefaultVideo.png', offscreen=True)
+    liz=xbmcgui.ListItem(title, offscreen=True)
     xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=title, listitem=liz, isFolder=True)
 elif action == 'getdetails':
     url=urllib.unquote_plus(params["url"])
@@ -37,6 +37,6 @@ elif action == 'getdetails':
     liz.setInfo('video',
         {'title': url
         })
+    liz.setArt({'icon': 'DefaultVideo.png'})
     xbmcplugin.setResolvedUrl(handle=int(sys.argv[1]), succeeded=True, listitem=liz)
-
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
